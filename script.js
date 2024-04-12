8bb1a08c1995fc64ae9560e0e05cc654d4a5a17c
Czowek <154842302+Czowek@users.noreply.github.com>
2024-04-12 02:25:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     let a = document.querySelector(".a");
     let d = document.getElementById("msg");
-  
+    let titleExpressions = [">.<", "x_x", "0_0", "ಠ_ಠ", "ʕ•ᴥ•ʔ"];
+    let currentIndex = 0;
+
     function clickHandler() {
-      document.removeEventListener("click", clickHandler);
-      setTimeout(function() {
-        d.remove();
-      }, 100);
-      a.classList.add("visible");
-      document.getElementById("music").play();
+        document.removeEventListener("click", clickHandler);
+        setTimeout(function() {
+            d.remove();
+        }, 100);
+        a.classList.add("visible");
+        document.getElementById("music").play();
     }
-  
+
+    function changeTitle() {
+        document.title = titleExpressions[currentIndex];
+        currentIndex = (currentIndex + 1) % titleExpressions.length;
+    }
+
     document.addEventListener("click", clickHandler);
-  });
-  
+    setInterval(changeTitle, 1000);
+});
