dafd73b42569d8da4ce7fc45528e36d0a73ceca3
Czowek <154842302+Czowek@users.noreply.github.com>
2024-04-13 16:12:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let titleExpressions = [">.< - Czowek.pl", "x_x - Czowek.pl", "0_0 - Czowek.pl", "ಠ_ಠ - Czowek.pl", "ʕ•ᴥ•ʔ - Czowek.pl"];
     let currentIndex = 0;
 
+    // Function to handle click event
     function clickHandler() {
         document.removeEventListener("click", clickHandler);
         setTimeout(function() {
@@ -13,11 +14,26 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("music").play();
     }
 
+    // Function to change title periodically
     function changeTitle() {
         document.title = titleExpressions[currentIndex];
         currentIndex = (currentIndex + 1) % titleExpressions.length;
     }
 
+    // Function to update visit count
+    function updateVisitCount() {
+        let count = localStorage.getItem("visitCount");
+        count = count ? parseInt(count) + 1 : 1;
+        document.getElementById("count").textContent = count;
+        localStorage.setItem("visitCount", count);
+    }
+
+    // Event listener for click event
     document.addEventListener("click", clickHandler);
+
+    // Interval for changing title
     setInterval(changeTitle, 1000);
+
+    // Update visit count on page load
+    updateVisitCount();
 });
